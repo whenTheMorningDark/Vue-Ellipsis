f00b9bde04f4d8b36fd9882a24f9e2b863c6dbb2
Mr <404792402@qq.com>
2024-04-02 15:20:42 +0800

    add:md

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, PluginOption } from "vite"
 import vue from "@vitejs/plugin-vue"
 import css from "rollup-plugin-css-only" // 引入插件
 export default defineConfig({
-  plugins: [vue(), css({ output: "bundle.css" }) as PluginOption],
+  plugins: [vue(), css() as PluginOption],
   build: {
     sourcemap: true, // 启用源代码映射
     lib: {
