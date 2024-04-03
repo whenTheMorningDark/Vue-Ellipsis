97c5a897659835dd5edfa8558eabd9e0db006c39
Mr <404792402@qq.com>
2024-04-03 14:22:49 +0800

    fix:修改依赖

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { createApp } from "vue"
 import App from "./App.vue"
-import ElementPlus from "element-plus"
 import vEllipsisPlugin from "../src/packages/index"
-import "element-plus/dist/index.css"
 import "../src/packages/style.scss"
 const app = createApp(App)
-app.use(ElementPlus)
 app.use(vEllipsisPlugin)
 app.mount("#app")
